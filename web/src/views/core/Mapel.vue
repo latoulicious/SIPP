e0af46a83781404e97aa547da13ce20f77d83452
latoulicious <arsyds@proton.me>
2024-01-19 15:04:10 +0700

    :sparkles: Feat: Implement views layout for core components

---
+++ web/src/views/core/Mapel.vue
@@ -0,0 +1,14 @@
+<script setup>
+import TableMapel from "@/components/core/TableMapel.vue";
+</script>
+
+<template>
+  <CorePageLayout>
+    <va-card class="scaled-card" stripe stripe-color="danger">
+      <va-card-title>Manage Mata Pelajaran</va-card-title>
+      <va-card-content>
+        <TableMapel />
+      </va-card-content>
+    </va-card>
+  </CorePageLayout>
+</template>
